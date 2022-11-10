1f60bbb77e336bb10aec041f7a2d8b99f41bec7d
Nata <erbet@mail.ru>
2022-11-10 20:26:58 +0300

    10/11 - xf cn слайдера

--- js/effects.js
+++ js/effects.js
@@ -6,6 +6,22 @@ const sliderElement = document.querySelector('.effect-level__slider');
 const effectLevel = document.querySelector('.effect-level__value');
 const DEFAULT_EFFECT = EFFECTS[0];
 let chosenEffect = DEFAULT_EFFECT;
+const isDefault = () => chosenEffect === DEFAULT_EFFECT;
+
+const updateSlider = () => {
+  sliderElement.classList.remove('hidden'); // не убирается класс??
+  sliderElement.noUiSlider.updateOptions({
+    range: {
+      min: chosenEffect.min,
+      max: chosenEffect.max,
+    },
+    start: chosenEffect.max,
+    step: chosenEffect.step,
+  });
+  if (isDefault()) {
+    sliderElement.classList.add('hidden');
+  }
+};
 
 // переключение фильтров
 inputEffectRadio.addEventListener('change', () => {
@@ -13,13 +29,35 @@ inputEffectRadio.addEventListener('change', () => {
 });
 
 const onFormChange = (evt) => {
-  console.log(evt);
   if (!evt.target.classList.contains('effects__radio')) {
     return;
   }
   chosenEffect = EFFECTS.find((effect) => effect.name === evt.target.value);
-  imgPreview.classList.add(`effects__preview--${chosenEffect.name}`);
+  //imgPreview.classList.add(`effects__preview--${chosenEffect.name}`);
+  updateSlider();
 };
+
+const onSliderUpdate = () => {
+  imgPreview.style.filter = 'none';
+  imgPreview.className = '';
+  effectLevel.value = '';
+  if (isDefault()) {
+    return;
+  }
+  const sliderValue = sliderElement.noUiSlider.get();
+  imgPreview.classList.add(`effects__preview--${chosenEffect.name}`);
+  imgPreview.style.filter = `${chosenEffect.style}(${sliderValue})`; //filter: invert(0..100%) с шагом 1%; ??
+};
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: DEFAULT_EFFECT.min,
+    max: DEFAULT_EFFECT.max,
+  },
+  start: DEFAULT_EFFECT.max,
+  step: DEFAULT_EFFECT.step,
+  connect: 'lower',
+});
 
 form.addEventListener('change', onFormChange);
 
